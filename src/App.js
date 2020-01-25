d08d427db72f11b94918637428ddc2147eaf4347
zena <syglee7@gmail.com>
2020-01-25 18:29:02 +0900

    basic react grammar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { HashRouter, Route } from 'react-router-dom';
+import About from './routes/About';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello world');
-  }
-  state = {
-    count: 0
-  };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-  componentDidMount() {
-    console.log("component rendered");
-  }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
-  render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
+function App() { 
+  return (
+  <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About}/>
+    <Route path="/movie/:id" component={Detail}/>
+  </HashRouter>
+  );
 }
 
 export default App;
