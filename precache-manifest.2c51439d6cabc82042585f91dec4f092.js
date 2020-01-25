8755cfebbb10e044c33c10e7aaaf01a52df55a51
zena <syglee7@gmail.com>
2020-01-25 16:19:42 +0900

    Updates

---
+++ precache-manifest.2c51439d6cabc82042585f91dec4f092.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b474f2d3b92116aab5bb30f09adfcbd0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b45b907885bcd01836e5",
+    "url": "/movie_app_2020/static/css/main.4cbd113e.chunk.css"
+  },
+  {
+    "revision": "9966fbb9ece634a56ad4",
+    "url": "/movie_app_2020/static/js/2.44bdb5fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.44bdb5fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b45b907885bcd01836e5",
+    "url": "/movie_app_2020/static/js/main.129c3624.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
